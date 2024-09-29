31fd0efca7d6ab1cc5b70b507e59e6b2fac99a37
Mockingjay_HrGs <noah.dunis@coda-student.school>
2024-09-29 13:14:22 +0200

    etape 5 : l'opération

--- main.c
+++ main.c
@@ -19,15 +19,33 @@ int main()
     scanf("%lf", &nombre2);
 
 	if (operateur == '+') {
-        printf("addition\n");
+        resultat = nombre1 + nombre2;
+        printf("Résultat de l'addition: %d\n", resultat);
+        
     } else if (operateur == '-') {
-        printf("soustraction\n");
+        resultat = nombre1 - nombre2;
+        printf("Résultat de la soustraction: %d\n", resultat);
+        
     } else if (operateur == '*') {
-        printf("multiplication\n");
+        resultat = nombre1 * nombre2;
+        printf("Résultat de la multiplication: %d\n", resultat);
+        
     } else if (operateur == '/') {
-        printf("division\n");
+        if (nombre2 == 0) {
+            printf("Erreur: Il est impossible de diviser par 0\n");
+        } else {
+            resultat_div = (double)nombre1 / nombre2;
+            printf("Résultat de la division: %.2lf\n", resultat_div);
+        }
+        
     } else if (operateur == '%') {
-        printf("modulo\n");
+        if (nombre2 == 0) {
+            printf("Erreur: Il est impossible de diviser par 0\n");
+        } else {
+            resultat = nombre1 % nombre2;
+            printf("Résultat du modulo: %d\n", resultat);
+        }
+        
     } else {
         printf("Opérateur non reconnu\n");
     }
