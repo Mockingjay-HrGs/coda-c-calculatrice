e40c73a94cc6fff6769f500250833b117b0654f1
Mockingjay_HrGs <noah.dunis@coda-student.school>
2024-09-29 13:02:16 +0200

    etape 3 : recupérer les infos

--- main.c
+++ main.c
@@ -1,11 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
+
+
+
 int main()
 {
-	
+    char operateur;
+    double nombre1, nombre2, resultat;
 
+    printf("Choisissez un opérateur parmi la liste suivante (+, -, *, /, %%): ");
+    scanf(" %c", &operateur);
 
-
+    printf("Entrez le premier nombre: ");
+    scanf("%lf", &nombre1);
+    printf("Entrez le second nombre: ");
+    scanf("%lf", &nombre2);
 	exit(0);
-}
+}	
+	
